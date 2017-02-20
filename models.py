43888d4423f5de742d9e7d0a5736bb8a8b2c7add
Dustin DAvignon <ddavignon@csumb.edu>
2017-02-20 17:06:10 +0000

    edit database variable

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import os, app
 from flask_sqlalchemy import SQLAlchemy
 
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dustin:potato@localhost/postgres'#os.getenv('DATABASE_URL')#
+app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')#'postgresql://dustin:potato@localhost/postgres'#
 db = SQLAlchemy(app.app)
 
 class Message(db.Model):
